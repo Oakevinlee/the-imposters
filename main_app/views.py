3e93e0ba8e1da4850b5cc949c6fcfb720cf9f2da
BobbyWasabiReal <wasabibob13@gmail.com>
2022-11-22 12:01:53 -0800

    Details page

--- main_app/views.py
+++ main_app/views.py
@@ -19,6 +19,21 @@ def recipes_index(request):
     'recipes': recipes
 })
 
+def recipes_detail(request, recipe_id):
+  recipe = Recipe.objects.get(id=recipe_id)
+  # First, create a list of the toy ids that the cat DOES have
+
+  # id_list = cat.toys.all().values_list('id')
+
+  # Query for the toys that the cat doesn't have
+  # by using the exclude() method vs. the filter() method
+  # toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  # instantiate FeedingForm to be rendered in detail.html
+  # feeding_form = FeedingForm()
+  return render(request, 'recipes/detail.html', {
+    'recipe': recipe
+  })
+
 class RecipeCreate(LoginRequiredMixin, CreateView):
   model = Recipe
   fields = ['name', 'ingredients', 'description', 'directions','region']
